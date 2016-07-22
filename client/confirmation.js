d80059397bf466dceeb48a8f5db0ec4a1c3a180e
Yacine <yacine@merzouk.ca>
2016-07-21 15:15:21 -1000

    1.0.6

--- client/confirmation.js
+++ client/confirmation.js
@@ -10,5 +10,11 @@ Template.confirmation.rendered = function(){
 Template.confirmation.helpers({
     total: function(){
         return ( this.transaction.amount / 100 ).toFixed(2);
-    }
+    },
+    refund: function(){
+        return this.adjustments && this.adjustments.refund ? ( this.adjustments.refund / 100 ).toFixed(2) : false;
+    },
+    correctedAmount: function(){
+    return this.adjustments && this.adjustments.correctedAmount ? ( this.adjustments.correctedAmount / 100 ).toFixed(2) : false;
+}
 });
