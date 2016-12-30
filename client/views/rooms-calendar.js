7f6f52ef9fa8b717515f1e0a6ebc248515e6cef3
Yacine <yacine@merzouk.ca>
2016-12-29 18:13:54 -1000

    1.3.26

--- client/views/rooms-calendar.js
+++ client/views/rooms-calendar.js
@@ -1,33 +1,86 @@
-Template.roomsCalendar.onCreated(function(){
-    // Meteor.subscribe('futureReservations'),
-    // Meteor.subscribe('rooms'),
-    // Meteor.subscribe('futureGames')
 
+Template.roomsCalendar.onRendered(function(){
+
+
+    if( !Session.get('calendarDay') ) {
+        Session.set('calendarDay', Epoch.dateObjectToDateString(new Date()));
+        console.log( 'SETTING CALENDAR DAY', Epoch.dateObjectToDateString(new Date()) );
+    }else{
+        console.log( 'CALENDAR DAY ALREADY SET', Session.get('calendarDay') );
+    }
+
+    this.autorun(function() {
+
+        Session.set('calendarReservationsReady',false);
+        Session.set('calendarGamesReady',false);
+
+        Meteor.subscribe(
+            'reservations',
+            Session.get('calendarDay'),
+            {
+                onReady: function () {
+                    console.log('reservations ready');
+                    Session.set('calendarReservationsReady', true);
+                }
+            }
+        );
+        Meteor.subscribe(
+            'games',
+            Session.get('calendarDay'),
+            {
+                onReady: function () {
+                    console.log('games ready');
+                    Session.set('calendarGamesReady', true);
+                }
+            }
+        );
+    });
 });
 Template.roomsCalendar.helpers({
-    days: function(){
-        var today = Epoch.dateObjectToDateString( new Date() );
-        var days = [];
-        var daysArray = [];
-        daysArray.push( Bolt.getCalendarDay( today ) );
-        days = daysArray;
-
-        for( var x = 0; x < 7; x++ ){
-            var prevDay = days[ daysArray.length - 1 ];
-            var nextDay = Epoch.addDaysToDate(1,prevDay.date);
-            days.push( Bolt.getCalendarDay(nextDay) );
+    // days: function(){
+    //     var today = Epoch.dateObjectToDateString( new Date() );
+    //     var days = [];
+    //     var daysArray = [];
+    //     daysArray.push( Bolt.getCalendarDay( today ) );
+    //     days = daysArray;
+    //
+    //     for( var x = 0; x < 7; x++ ){
+    //         var prevDay = days[ daysArray.length - 1 ];
+    //         var nextDay = Epoch.addDaysToDate(1,prevDay.date);
+    //         days.push( Bolt.getCalendarDay(nextDay) );
+    //     }
+    //     console.log( 'DAYS', days );
+    //     return days;
+    // },
+    day: function(){
+        var day = Bolt.getCalendarDay( Session.get('calendarDay') );
+        if( Session.get('calendarReservationsReady') && Session.get('calendarGamesReady') ){
+            console.log( 'Everything ready; returning day', day );
+            return day;
+        }else{
+            console.log( 'Everything NOT ready; returning false' );
+            return false;
         }
-        console.log( 'DAYS', days );
-        return days;
     }
 });
 
-// Template.roomsCalendar.events({
-//     'click [hook="set-time"]': function(evt,tmpl){
-//         evt.preventDefault();
-//         var url = $(evt.currentTarget).attr('href');
-//         Session.set( 'selectedTime', $(evt.currentTarget).attr('hook-data') );
-//         // console.log( evt, evt.currentTarget );
-//         Router.go( url );
-//     }
-// });
+Template.roomsCalendar.events({
+    'click [hook="set-time"]': function(evt,tmpl){
+        // evt.preventDefault();
+        Session.set( 'selectedTimeFromCalendar', $(evt.currentTarget).attr('hook-data') );
+        console.log( "SETTING TIME FOR CALENDAR", evt.currentTarget );
+        // Router.go( url );
+    },
+    'click [hook="add-day"]': function(evt,tmpl){
+        // console.log(Session.get('calendarDay'),'Adding 1 day',Epoch.addDaysToDate(1, Session.get('calendarDay')));
+        evt.preventDefault();
+        var updatedCalendarDay = Epoch.addDaysToDate(1, Session.get('calendarDay'));
+        Session.set( 'calendarDay', updatedCalendarDay );
+    },
+    'click [hook="subtract-day"]': function(evt,tmpl){
+        // console.log(Session.get('calendarDay'),'Adding -1 day',Epoch.addDaysToDate(-1,Session.get('calendarDay')));
+        evt.preventDefault();
+        var updatedCalendarDay = Epoch.addDaysToDate(-1,Session.get('calendarDay'));
+        Session.set( 'calendarDay', updatedCalendarDay );
+    }
+});
